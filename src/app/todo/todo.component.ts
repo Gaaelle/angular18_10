35825fe13a5fc022923cf3da241d8df6a4d3f09b
gaaelle <gaaelle.robart@gmail.com>
2017-10-19 12:44:27 +0200

    pause midi :D

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-todo',
@@ -6,12 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  tasks = [];
+  taskText = '';
+  tasks: Task[] = [];
+  change= "";
 
-  tasks.push({title: 'ma tache'});
   constructor() { }
 
   ngOnInit() {
   }
 
+  addTask(event: KeyboardEvent) {
+    if (event.keyCode === 13 && this.taskText.trim() !== '') {
+      this.tasks.push(new Task(this.taskText, false));
+      this.taskText = ''; //pour vider l'input
+    }
+  }
+
+  removeTask(index): void {
+    this.tasks.splice(index, 1);
+  }
+
+  tasksDone(): number {
+    let total = 0;
+      for (let task of this.tasks) {
+        if(task.done === true) {
+          total ++;
+        }
+      }
+      return total;
+  }
 }
